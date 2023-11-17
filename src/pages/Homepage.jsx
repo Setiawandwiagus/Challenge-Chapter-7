c0105cd7ee3d88bff961da69a35a611207ad7bfb
Dwi Agus S <dwiagus435@gmail.com>
2023-11-17 13:40:31 +0700

    update name

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Homepage = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center text-3xl w-full h-screen bg-blue-400 text-white ">
+        DWI AGUS SETIAWAN
+      </div>
+    </div>
+  );
+};
